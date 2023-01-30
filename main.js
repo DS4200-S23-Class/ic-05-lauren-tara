8a1e385f52efe362cdd8a192daf4d67db7d0676f
tarajoshii <119441296+tarajoshii@users.noreply.github.com>
2023-01-30 16:11:22 -0500

    button

---
+++ main.js
@@ -0,0 +1,10 @@
+function buttonClicked() {
+	console.log("button was clicked!");
+
+	let newText = "BUTTON WAS CLICKED";
+
+	let buttonDiv = document.getElementByID("button-div;");
+
+	buttonDiv.innerHTML = newText
+
+}
